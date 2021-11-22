45231f21b838119870f1977dc73e286bef4a2d9f
GH0STAV0 <you@example.com>
2021-11-21 17:23:22 -0800

    add new project

--- src/package.sh
+++ src/package.sh
@@ -42,8 +42,8 @@ echo "export PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[0
 #chmod +x /root/install_add/geckodriver && cp /root/install_add/geckodriver /usr/bin/geckodriver13
 echo "Install Package PIP-3 ***** pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4 selenium  ConfigParser lxml  speechrecognition requests pyvirtualdisplay pydub"
 #pip3 install selenium==4.0.0.a1
-#pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4   ConfigParser lxml  speechrecognition requests \
- pyvirtualdisplay pydub pyautogui emojis emoji mysql-connector
+#pip3 install pymysql pyvirtualdisplay faker-e164 Faker PySocks stem  bs4  ConfigParser lxml  speechrecognition requests \
+ #pyvirtualdisplay pydub pyautogui emojis emoji mysql-connector
 
 
 echo "Install Package OPENVPN TUN/TAP"
